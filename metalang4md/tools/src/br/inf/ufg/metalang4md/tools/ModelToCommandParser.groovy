ed55bcd6ce416b03f8bcbf7dae710c88db38cb0d
Adalberto Junior <adalrsjr1@gmail.com>
2019-04-06 20:41:03 -0300

    update ModelToCommandParser skeleton

--- metalang4md/tools/src/br/inf/ufg/metalang4md/tools/ModelToCommandParser.groovy
+++ metalang4md/tools/src/br/inf/ufg/metalang4md/tools/ModelToCommandParser.groovy
@@ -5,6 +5,10 @@ import org.eclipse.emf.compare.DifferenceKind
 import org.eclipse.emf.compare.internal.spec.AttributeChangeSpec
 import org.eclipse.emf.ecore.EObject
 
+import metalang4md.EActor
+import metalang4md.EDomainSpecificType
+import metalang4md.EItem
+
 class ModelToCommandParser {
 
 	public Object process(Diff diff) {
@@ -21,8 +25,59 @@ class ModelToCommandParser {
 
 class CommandFactory {
 	static Command create(DifferenceKind kind, EObject value, EObject parent) {
-
+		return doCreateByKind(kind, value, parent)
 	}
+	
+	static private Command doCreateByKind(DifferenceKind kind, EObject value, EObject parent) {
+		 switch(kind) {
+			 case DifferenceKind.ADD:
+			 return createAdd(value, parent)
+			 break
+			 
+			 case DifferenceKind.DELETE:
+			 return createDelete(value, parent)
+			 break
+			 
+			 case DifferenceKind.CHANGE:
+			 retrun createChange(value, parent)
+			 break
+			 
+			 case DifferenceKind.MOVE:
+			 throw new UnsupportedOperationException("There is no command to hande MOVE operation on model")
+			 break
+			 
+			 default:
+			 break
+		 }
+	}
+	
+	static private Command createAdd(EObject value, EObject parent) {
+	} 
+	
+	static private Command createDelete(EObject value, EObject parent) {
+	}
+	
+	static private Command createChange(EObject value, EObject parent) {
+	}
+	
+	static private Command createBind(EObject value, EObject parent) {
+	}
+
+	static private Command doCreateByType(EObject value, EObject parent) {
+		if(value instanceof EDomainSpecificType) {
+			
+		}
+		else if(value instanceof EActor) {
+			
+		}
+		else if(value instanceof EItem) {
+			
+		}
+		else {
+			
+		}
+	}
+		
 }
 
 interface Command {
@@ -50,4 +105,15 @@ class CommandChange implements Command {
 	@Override
 	public Object execute() {
 		return null;
-	} }
+	} 
+}
+
+class CommandBind implements Command {
+
+	@Override
+	public Object execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
