96d0b623a4e3be6be9fa08a32b064a172577c8f1
Adalberto Junior <adalrsjr1@gmail.com>
2019-04-19 17:46:05 -0300

    fix diffs sorting

--- metalang4md/src/main/java/br/ufg/inf/synthesis/Ml4mdModelComparator.groovy
+++ metalang4md/src/main/java/br/ufg/inf/synthesis/Ml4mdModelComparator.groovy
@@ -1,7 +1,9 @@
 package br.ufg.inf.synthesis
 
-import br.ufg.inf.metalang4md.cml.CmlPackage
+
 import br.ufg.inf.metalang4md.EDomainSpecificElement
+import br.ufg.inf.metalang4md.cml.CmlPackage
+import com.google.common.collect.ComparisonChain
 import org.eclipse.emf.common.util.URI
 import org.eclipse.emf.compare.Comparison
 import org.eclipse.emf.compare.Diff
@@ -16,6 +18,7 @@ import org.eclipse.emf.compare.merge.IMerger
 import org.eclipse.emf.compare.scope.DefaultComparisonScope
 import org.eclipse.emf.compare.scope.IComparisonScope
 import org.eclipse.emf.compare.utils.UseIdentifiers
+import org.eclipse.emf.ecore.EObject
 import org.eclipse.emf.ecore.EPackage
 import org.eclipse.emf.ecore.resource.Resource
 import org.eclipse.emf.ecore.resource.ResourceSet
@@ -24,105 +27,134 @@ import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl
 
 class Ml4mdModelComparator {
 
-	static Tuple3<Resource, Resource, Comparison> compare(File newModel, File oldModel) {
-		ResourceSet newResourceSet = new ResourceSetImpl()
-		ResourceSet oldResourceSet = new ResourceSetImpl()
+    static Tuple2<Resource, Comparison> compare(File newModel, File oldModel) {
+        ResourceSet newResourceSet = new ResourceSetImpl()
+        ResourceSet oldResourceSet = new ResourceSetImpl()
 
-		String xmi1 = newModel.absolutePath
-		String xmi2 = oldModel.absolutePath
+        String xmi1 = newModel.absolutePath
+        String xmi2 = oldModel.absolutePath
 
-		def newResource = load(xmi1, newResourceSet)
-		def oldResource = load(xmi2, oldResourceSet)
+        def newResource = load(xmi1, newResourceSet)
+        def oldResource = load(xmi2, oldResourceSet)
 
-		IEObjectMatcher matcher = DefaultMatchEngine.createDefaultEObjectMatcher(UseIdentifiers.NEVER)
-		IComparisonFactory comparisonFactory = new DefaultComparisonFactory(new DefaultEqualityHelperFactory())
-		IMatchEngine.Factory matchEngineFactory = new MatchEngineFactoryImpl(matcher, comparisonFactory)
-		matchEngineFactory.setRanking(20)
-		IMatchEngine.Factory.Registry matchEngineRegistry = new MatchEngineFactoryRegistryImpl()
-		matchEngineRegistry.add(matchEngineFactory)
-		EMFCompare comparator = EMFCompare.builder().setMatchEngineFactoryRegistry(matchEngineRegistry).build()
+        return compare(newResource, newResourceSet, oldResource, oldResourceSet)
+    }
 
-		IComparisonScope scope = new DefaultComparisonScope(newResourceSet, oldResourceSet, null)
+    static Tuple2<Resource, Comparison> compare(Resource newResource, ResourceSet newResourceSet, Resource oldResource, ResourceSet oldResourceSet) {
+        IEObjectMatcher matcher = DefaultMatchEngine.createDefaultEObjectMatcher(UseIdentifiers.NEVER)
+        IComparisonFactory comparisonFactory = new DefaultComparisonFactory(new DefaultEqualityHelperFactory())
+        IMatchEngine.Factory matchEngineFactory = new MatchEngineFactoryImpl(matcher, comparisonFactory)
+        matchEngineFactory.setRanking(20)
+        IMatchEngine.Factory.Registry matchEngineRegistry = new MatchEngineFactoryRegistryImpl()
+        matchEngineRegistry.add(matchEngineFactory)
+        EMFCompare comparator = EMFCompare.builder().setMatchEngineFactoryRegistry(matchEngineRegistry).build()
 
-		return new Tuple3(newResource, oldResource, comparator.compare(scope))
-	}
+        IComparisonScope scope = new DefaultComparisonScope(newResourceSet, oldResourceSet, null)
 
-	static Resource load(String absolutePath, ResourceSet resourceSet) {
-		EPackage.Registry.INSTANCE.put(CmlPackage.eNS_URI, CmlPackage.eINSTANCE)
+        return new Tuple2(oldResource, comparator.compare(scope))
+    }
 
-		URI uri = URI.createFileURI(absolutePath)
+    private static sortingDifferences(List<Diff> differences) {
+        def and = { List list ->
+            list.inject { acc, val -> acc && val }
+        }
 
-		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl())
+        def xor = { List list ->
+            list.inject { acc, val -> acc ^ val }
+        }
 
-		return resourceSet.getResource(uri, true)
-	}
+        return differences.sort { Diff arg0, Diff arg1 ->
+            // decreasing sort
+            def elem0 = arg0.value
+            def elem1 = arg1.value
 
-	static void save(String savePath, Resource resource) {
-		OutputStream os = new FileOutputStream(new File(savePath))
-		
-		try {
-			resource.save(os, Collections.EMPTY_MAP);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		finally {
-			os.close();
-		}
-	}
-	
-	static void main(String[] args) {
-		def oldModel = new File("model/metamodel/cml-model/cml-model2.xmi")
-		def newModel = new File("model/metamodel/cml-model/cml-model3.xmi")
+            ComparisonChain chain = ComparisonChain.start()
 
-		def (newResource, oldResource, comparison) = compare(newModel, oldModel)
+            if (and([elem0 instanceof EDomainSpecificElement, elem1 instanceof EDomainSpecificElement])) {
+                // comparision by priority
+                chain = chain.compare(elem1.commandPriority, elem0.commandPriority)
+            } else if (and([elem0 instanceof EObject, elem1 instanceof EObject])) {
+                // comparision by # of children
+                chain = chain.compare(elem1.eContents().size(), elem0.eContents().size())
+            } else if (xor([elem0 instanceof EObject, elem1 instanceof EObject])) {
+                // comparision by # of children  -- EObject.compareTo(primitiveType)
+                EObject eObject = elem1 instanceof EObject ? elem1 : elem0
+                chain = chain.compare(0, eObject.eContents().size())
+            } else {
+                // comparision by natural ordering
+                chain = chain.compare(elem1, elem0)
+            }
 
-		List<Diff> differences = comparison.getDifferences()
-		
-//		Ordering ordering = new Ordering() {
-//
-//			@Override
-//			public int compare(def arg0, def arg1) {
-//				def elem0 = null
-//				def elem1 = null
-//				if(arg0 instanceof EDomainSpecificElement) {
-//					elem0 = arg0
-//				}
-//
-//				if(arg1 instanceof EDomainSpecificElement) {
-//					elem1 = arg1
-//				}
-//
-//				if(elem0 != null && elem1 != null) {
-//					return ComparisonChain.start()
-//					.compare(elem0.priority, elem1.priority)
-//					.result()
-//				}
-//				return 0
-//
-//			}
-//
-//		}
-//
-//		differences = ordering.sortedCopy(differences)
-		
-		differences.each { Diff diff ->
+            return chain.result()
+        }
+    }
+
+    static Resource load(String absolutePath, ResourceSet resourceSet) {
+        EPackage.Registry.INSTANCE.put(CmlPackage.eNS_URI, CmlPackage.eINSTANCE)
+
+        URI uri = URI.createFileURI(absolutePath)
+
+        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl())
+
+        return resourceSet.getResource(uri, true)
+    }
+
+    static void save(String savePath, Resource resource) {
+        OutputStream os = new FileOutputStream(new File(savePath))
+
+        try {
+            resource.save(os, Collections.EMPTY_MAP);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            os.close();
+        }
+    }
+
+    static List<Diff> computeChangesAndSave(String pathnameModel1, String pathnameModel2, String pathnameNewModel) {
+        def model1 = new File(pathnameModel1)
+        def model2 = new File(pathnameModel2)
+
+        def (newModelResource, comparison) = compare(model2, model1)
+
+        List differences = sortingDifferences(comparison.getDifferences())
+
+        IMerger.Registry mergerRegistry = IMerger.RegistryImpl.createStandaloneInstance()
+        IBatchMerger merger = new BatchMerger(mergerRegistry)
+        merger.copyAllLeftToRight(differences, null)
+
+        save(pathnameNewModel, newModelResource)
+
+        return differences
+    }
+
+    static List<Diff> computeChanges(String pathnameModel1, String pathnameModel2) {
+        def model1 = new File(pathnameModel1)
+        def model2 = new File(pathnameModel2)
+
+        def (newModelResource, comparison) = compare(model2, model1)
+
+        return sortingDifferences(comparison.getDifferences())
+    }
+
+    static void main(String[] args) {
+        String empty = "model/metamodel/cml-model/ControlSchemaEmpty.xmi"
+        String version1 = "model/metamodel/cml-model/ControlSchemaTwoWay.xmi"
+
+
+        List<Diff> differences = computeChanges(empty, version1)
+
+        differences.each { Diff diff ->
 //			println ${diff.attribute.eContainer()} ${diff.value}
-			println diff
-			println ">> ${diff.kind} ${diff.value}"
-			if((diff.value instanceof EDomainSpecificElement)) {
+//			println diff
+            println ">> ${diff.kind} ${diff.value}"
+            if ((diff.value instanceof EDomainSpecificElement)) {
 //				print ">> ${diff.reference.eContainer()}"
 //				println ">> ${diff.attribute.eContainer()}"
-			} 
+            }
 //			def value = diff.value
 //			println value instanceof EItem
-		}
-		
-		IMerger.Registry mergerRegistry = IMerger.RegistryImpl.createStandaloneInstance()
-		IBatchMerger merger = new BatchMerger(mergerRegistry)
-		merger.copyAllLeftToRight(differences, null)
-
-		save("model/metamodel/cml-model/cml-merged.xmi", oldResource)
-
-
-	}
+        }
+    }
 }
