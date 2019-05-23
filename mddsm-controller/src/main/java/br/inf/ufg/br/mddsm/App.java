b875dfd0d8c5d00ecd8a3b0dd1ed4c628f74758d
Adalberto Junior <adalrsjr1@gmail.com>
2019-05-23 18:54:25 +0000

    add projects related to controller

---
+++ mddsm-controller/src/main/java/br/inf/ufg/br/mddsm/App.java
@@ -0,0 +1,13 @@
+package br.inf.ufg.br.mddsm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
