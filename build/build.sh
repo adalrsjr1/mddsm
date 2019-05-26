fb71fd6cb33f46b0926f5ebf1a439bb355146eb9
Adalberto Junior <adalrsjr1@gmail.com>
2019-05-26 12:27:13 -0300

    docker builder added to project

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 func() {
-  docker run -it --rm -v "$(dirname $PWD)":/home/builder -v "$HOME/.m2":/root/.m2   adalrsjr1/mddsm-builder mvn $1
-  rm -rf builder
+  docker run -it --rm  -e MAVEN_CONFIG=/var/maven/.m2 -v "$(dirname $PWD)":/home/builder -v "$HOME/.m2":/var/maven/.m2 adalrsjr1/mddsm-builder mvn -Duser.home=/var/maven $1
+  rm -rf builder ../?/
 }
 
 clean() {
